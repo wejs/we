2adcaaa6dd4ab954a14ad46d606386735043e008
Feel Fine <danielfeelfine@gmail.com>
2014-01-02 12:46:04 -0300

    Create model Links

---
+++ api/models/Links.js
@@ -0,0 +1,24 @@
+/**
+ * Links
+ *
+ * @module      :: Model
+ * @description :: Link content
+ *
+ */
+
+module.exports = {
+
+  attributes: {
+
+    active: 'boolean',
+
+    url: {
+        type: 'url'
+    },
+
+    name: {
+        type: 'STRING'
+    }
+  }
+
+};
