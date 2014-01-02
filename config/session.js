820efe9977284181679a3715f91fd12f053e92bf
Feel Fine <danielfeelfine@gmail.com>
2014-01-02 14:21:41 -0300

    Add mongo settings in config sessions

--- config/session.js
+++ config/session.js
@@ -15,7 +15,7 @@ module.exports.session = {
   // Session secret is automatically generated when your new app is created
   // Replace at your own risk in production-- you will invalidate the cookies of your users,
   // forcing them to log in again. 
-  secret: '83909b538a637916fd58b07a4218b8bd'
+  secret: '83909b538a637916fd58b07a4218b8bd',
 
 
   // In production, uncomment the following lines to set up a shared redis session store
@@ -33,6 +33,15 @@ module.exports.session = {
   // pass: <redis auth password>
   // prefix: 'sess:'
 
+  adapter: 'mongo',
+  host: 'localhost',
+  port: 27017,
+  db: 'we_sails',
+  collection: 'sessions',
+  auto_reconnect: true,
+  cookie: {
+    originalMaxAge: 2592000000 // +-30 days
+  }
 
   // Uncomment the following lines to use your Mongo adapter as a session store
   // adapter: 'mongo',
