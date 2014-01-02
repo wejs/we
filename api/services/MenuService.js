c365ee2043ee04c2aaaf817a9111626e6cb793df
Feel Fine <danielfeelfine@gmail.com>
2014-01-02 13:19:54 -0300

    Add MenuService

---
+++ api/services/MenuService.js
@@ -0,0 +1,50 @@
+exports.getMenu = function(menuMame,req) {
+  var menuRendered = '';
+  var menu = {};
+  var currentUrl = '';
+  //console.log(this);
+  // TODO add suport to metatags
+  var mainMenu = {
+    name: 'mainMenu',
+    id: 'main-menu',
+    links: [
+    {
+      title: 'Dashboard',
+      url: '/dashboard'
+    },
+    {
+      title: 'Activity',
+      url: '/activity'
+    },
+    {
+      title: 'Users',
+      url: '/users'
+    }
+  ]};
+
+  switch(menuMame)
+  {
+  case 'userMenu':
+    menu = userMenu;
+    break;
+  default:
+    menu = mainMenu;
+  }
+
+  menuRendered += '<ul class="nav navbar-nav '+ menu.id +'" >';
+  menu.links.forEach(function (link){
+    linkClass = '';
+    if (req.url == link.url ) linkClass = 'active';
+
+    menuRendered += '<li class="menu-item '+ linkClass +'">';
+
+    menuRendered += '<a href="'+ link.url +'">'+ link.title + '</a>';
+    menuRendered += '</li>';
+  });
+  menuRendered += '<li><a data-toggle="modal" href="#fileManagerModal" >'+req.res.i18n('File Manager') + '</a></li>';
+
+  menuRendered += '</ul>';
+
+  return menuRendered;
+
+};
