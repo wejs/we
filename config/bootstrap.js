e221c4d421ba9384137f4621d85678c5b6ff9aef
Feel Fine <danielfeelfine@gmail.com>
2014-01-02 13:54:58 -0300

    Add bootstrap config with user avatar mecanism

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -10,7 +10,50 @@
 
 module.exports.bootstrap = function (cb) {
 
-  // It's very important to trigger this callack method when you are finished 
+  sails.io.on('connection', function(socket) {
+
+    var userId = socket.handshake.session.passport.user;
+    if(userId){
+      if(typeof sails.onlineusers === 'undefined' )
+        sails.onlineusers = {};
+
+      // save user data in online users cache
+      if(typeof sails.onlineusers[userId] === 'undefined' ){
+        Users.findOneById(userId).done(function(err, user){
+              console.log(sails.onlineusers);
+          sails.onlineusers[userId] = user.toJSON();
+        });
+
+      }
+
+      // join user exclusive room to allow others users send
+      // mesages to this user
+      // Users.subscribe(socket , [userId] );
+      socket.join('user_' + userId);
+
+    }
+    // TODO change to userId friends room
+    socket.join('global');
+
+    socket.on('disconnect', function () {
+        console.log('Disconect!!! ');
+        if(userId){
+          // TODO change to send to friends
+          sails.io.sockets.in('global').emit('contact:disconnect', {
+              status: 'disconected',
+              contact: {
+                id: userId
+              }
+          });
+
+          console.log('sned Disconect!!! ');
+          // remove user from users online
+          delete sails.onlineusers[userId];
+        }
+
+    });
+  });
+  // It's very important to trigger this callack method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   cb();
 };
