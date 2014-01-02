a7eb64d863f567a72b876605fe9f844dd6e45466
Feel Fine <danielfeelfine@gmail.com>
2014-01-02 13:50:07 -0300

    Change adapters do sails-mongo config

--- config/adapters.js
+++ config/adapters.js
@@ -1,8 +1,8 @@
 /**
  * Global adapter config
- * 
+ *
  * The `adapters` configuration object lets you create different global "saved settings"
- * that you can mix and match in your models.  The `default` option indicates which 
+ * that you can mix and match in your models.  The `default` option indicates which
  * "saved setting" should be used if a model doesn't have an adapter specified.
  *
  * Keep in mind that options you define directly in your model definitions
@@ -14,26 +14,48 @@
 
 module.exports.adapters = {
 
-  // If you leave the adapter config unspecified 
+  // If you leave the adapter config unspecified
   // in a model definition, 'default' will be used.
-  'default': 'disk',
+  'default': 'mongo',
+
+  // In-memory adapter for DEVELOPMENT ONLY
+  memory: {
+    module: 'sails-memory'
+  },
 
   // Persistent adapter for DEVELOPMENT ONLY
-  // (data is preserved when the server shuts down)
+  // (data IS preserved when the server shuts down)
+  /*
   disk: {
     module: 'sails-disk'
   },
+  */
 
+  mongo: {
+    module   : 'sails-mongo',
+    url      : 'mongodb://localhost:27017/we_sails'
+  },
+
+  // MongoAdapter
+  // https://github.com/balderdashy/sails-mongo
+  /*
+  mongo: {
+    module    : 'sails-mongo',
+    host    : 'localhost',
+    port    : '27017',
+    database  : 'we_sails'
+  },
+*/
   // MySQL is the world's most popular relational database.
   // Learn more: http://en.wikipedia.org/wiki/MySQL
-  myLocalMySQLDatabase: {
+  mysql: {
 
     module: 'sails-mysql',
     host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
     user: 'YOUR_MYSQL_USER',
     // Psst.. You can put your password in config/local.js instead
     // so you don't inadvertently push it up if you're using version control
-    password: 'YOUR_MYSQL_PASSWORD', 
+    password: 'YOUR_MYSQL_PASSWORD',
     database: 'YOUR_MYSQL_DB'
   }
 };
