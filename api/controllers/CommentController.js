6a70d0d20ba3d5822ce8fd9923196651ebfb76ff
Feel Fine <danielfeelfine@gmail.com>
2014-01-02 13:28:57 -0300

    Create CommentController

---
+++ api/controllers/CommentController.js
@@ -0,0 +1,17 @@
+/**
+ * CommentController
+ *
+ * @module		:: Controller
+ * @description	:: Contains logic for handling requests.
+ */
+
+module.exports = {
+
+  /* e.g.
+  sayHello: function (req, res) {
+    res.send('hello world!');
+  }
+  */
+  
+
+};
