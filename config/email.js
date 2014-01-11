67a4e497b558891b611e5b45c358d5da2ff1befb
Alberto Souza <alberto.souza.99@gmail.com>
2014-01-11 18:48:23 -0200

    email config example added

---
+++ config/email.js
@@ -0,0 +1,26 @@
+
+// Email config example
+/*
+module.exports.email = {
+  from_email: 'We CMS',
+  from_name:  'services@we-cms.org',
+  defaultService: 'Live',
+
+  services: {
+    Live: {
+      service: "Live",
+      auth: {
+        user: "email@server.com",
+        pass: "password"
+      }       
+    },
+    gmail: {
+      service: "Gmail",
+      auth: {
+        user: "we-cms@gmail.com",
+        pass: "password"
+      }
+    }      
+  }
+};
+*/
