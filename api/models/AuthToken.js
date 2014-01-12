5475923f0fbf24ce4415d79b994eaa5c240b0ae5
Alberto Souza <alberto.souza.99@gmail.com>
2014-01-11 22:42:26 -0200

    AuthToken added to generate and save auth tokens for user activation, password reset and one time logins

---
+++ api/models/AuthToken.js
@@ -0,0 +1,29 @@
+/**
+ * AuthToken
+ *
+ * @module      :: Model
+ * @description :: Auth Token model for create login, password and activate account tokens
+ *
+ */
+var uuid = require('node-uuid');
+
+module.exports = {
+
+  attributes: {
+
+    token: {
+      type: 'string'
+    },
+
+    user_id: {
+      type: 'string',
+      required: true
+    }
+  },
+
+  beforeCreate: function(token, next) {
+    token.token = uuid.v1();
+    next();
+  }
+
+};
