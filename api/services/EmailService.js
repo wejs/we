7f4d3a8eec2bff130de614406bf5aa31e98de9c1
Alberto Souza <alberto.souza.99@gmail.com>
2014-01-11 22:41:23 -0200

    MailService.sendAccontActivationEmail added to send activation emails and MailService.sendMail changed to work with configs

--- api/services/EmailService.js
+++ api/services/EmailService.js
@@ -2,51 +2,160 @@
 // from https://github.com/balderdashy/sails-docs/blob/0.9/services.md
 
 var nodemailer = require("nodemailer");
-
-// create reusable transport method (opens pool of SMTP connections)
-var smtpTransport = nodemailer.createTransport("SMTP",{
-    service: "Live",
-    auth: {
-        user: "services@we-cms.org",
-        pass: "AnPaSs3*4mp13"
-    }
-});
+//var templatesDir   = path.resolve(__dirname, '..', 'views/mailer');
+var emailTemplates = require('email-templates');
 
 exports.sendInviteEmail = function(options) {
+  var smtpTransport;
 
-    var opts = {"type":"messages","call":"send","message":
-        {
-            "subject": "We CSMS ;)",
-            "from_email": "services@we-cms.org",
-            "from_name": "We CMS",
-            "to":[
-                {"email": options.email, "name": options.name}
-            ],
-            "text": "Dear "+options.name+",\nYou're in the Beta! Click <insert link> to verify your account"
-        }
-    };
+  smtpTransport = EmailService.getServerTransport(options);
+
+  var opts = {"type":"messages","call":"send","message":
+    {
+      "subject": "We CSMS ;)",
+      "from_email": "services@we-cms.org",
+      "from_name": "We CMS",
+      "to":[
+          {"email": options.email, "name": options.name}
+      ],
+      "text": "Dear "+options.name+",\nYou're in the Beta! Click <insert link> to verify your account"
+    }
+  };
 
 
     // setup e-mail data with unicode symbols
-    var mailOptions = {
-        from: sails.config.appName + " <" + sails.config.siteEmail + ">", // sender address
-        to: options.email, // list of receivers
-        subject: "Hello", // Subject line
-        text: "Hello world", // plaintext body
-        html: "<b>Hello world</b>" // html body
+  var mailOptions = {
+    from: sails.config.appName + " <" + sails.config.siteEmail + ">", // sender address
+    to: options.email, // list of receivers
+    subject: "Hello", // Subject line
+    text: "Hello world", // plaintext body
+    html: "<b>Hello world</b>" // html body
+  };
+
+  // send mail with defined transport object
+  /*
+  smtpTransport.sendMail(mailOptions, function(error, response){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Message sent: " + response.message);
     }
 
-    // send mail with defined transport object
-    smtpTransport.sendMail(mailOptions, function(error, response){
-        if(error){
-            console.log(error);
-        }else{
-            console.log("Message sent: " + response.message);
-        }
+    // if you don't want to use this transport object anymore, uncomment following line
+    //smtpTransport.close(); // shut down the connection pool, no more messages
+  });
+*/
+  //myEmailSendingLibrary.send(opts);
+};
 
-        // if you don't want to use this transport object anymore, uncomment following line
-        //smtpTransport.close(); // shut down the connection pool, no more messages
+exports.sendAccontActivationEmail = function(user, siteBaseUrl, cb){
+  var smtpTransport;
+
+  AuthToken.create( {user_id: user.id} ).done(function(error, token) {
+    if(error) return cb(error);
+    
+    var options = {};
+
+    options.email = user.email;
+    options.subject = 'WE Account activation email';
+
+    var templateVariables = {
+      user: {
+        name: user.name
+      },
+      site: {
+        name: 'WE-CMS',
+        slogan: 'MIMI one slogan here'
+      },
+      confirmUrl: siteBaseUrl + '/user/'+ user.id +'/activate/' + token.token
+    };
+
+    var templateName = 'AccontActivationEmail';
+    options.subject = 'WE-CMS -> Register validation email :)';
+
+    EmailService.sendEmail(options ,templateName ,templateVariables, cb);
+  });
+
+};
+
+exports.getServerTransport = function(serviceName) {
+  var emailServerTransport;
+  var emailServerConfig;
+  //console.log(sails.util);
+  if(!serviceName){
+
+    if( !sails.util.isUndefined(sails.config.email) )
+      if( !sails.util.isUndefined( sails.config.email.defaultService ) ){
+        serviceName = sails.config.email.defaultService;
+      }
+  }
+
+  if(serviceName) {
+    if( !sails.util.isUndefined(sails.config.email.services) ){
+
+      if( !sails.util.isUndefined( sails.config.email.services[serviceName] ) ){
+        emailServerConfig = sails.config.email.services[serviceName];
+        // create reusable transport method (opens pool of SMTP connections)
+        emailServerTransport = nodemailer.createTransport(
+          emailServerConfig['type'],emailServerConfig
+        );
+
+      }else{
+        // error, service not found
+        sails.log.error('Email service not found in config check if sails.config.email.services[serviceName exists');
+      }
+
+    } else {
+      sails.log.error('Email services not found in config');
+    }
+
+    // TODO add local email config to default
+    
+  }
+
+  return emailServerTransport;
+};
+
+exports.sendEmail = function(options, templateName, templateVariables, cb) {
+
+  var templatesDir = sails.config.paths.views + '/mailer';
+  emailTemplates(templatesDir, function(err, template) {
+    if (err) return cb(err, null);
+
+    // ## Send a single email
+
+    // Prepare nodemailer transport object
+    var transport = EmailService.getServerTransport();
+
+    // Send a single email
+    template(templateName, templateVariables, function(err, html, text) {
+      if (err) return cb(err, null);
+
+      if( sails.config.environment == 'test' ){
+        // dont send emails in test enviroment
+        console.info('To:\n',options.email);
+        console.info('Text:\n',html);
+        cb();        
+      } else { 
+
+        transport.sendMail({
+          from: sails.config.appName +' <' + sails.config.email.site_email + '>',
+          to: options.email,
+          subject: options.subject,
+          html: html,
+          // generateTextFromHTML: true,
+          text: text
+        }, function(err, responseStatus) {
+          if (err) return cb(err, null);
+          
+          sails.log.info('EmailService',responseStatus.message);
+
+          cb(null,responseStatus);
+        });
+
+      }
+
     });
 
-    //myEmailSendingLibrary.send(opts);
+  });
 };
