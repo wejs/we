931994d330ae6caf746f1ee7d71f036e95d6d04e
Alberto Souza <alberto.souza.99@gmail.com>
2014-02-01 17:22:20 -0200

    changed styles to old WE version while we dont have a working template engine

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,13 @@
 /**
  * Gruntfile
  *
- * If you created your Sails app with `sails new foo --linker`, 
+ * If you created your Sails app with `sails new foo --linker`,
  * the following files will be automatically injected (in order)
  * into the EJS and HTML files in your `views` and `assets` folders.
  *
  * At the top part of this file, you'll find a few of the most commonly
  * configured options, but Sails' integration with Grunt is also fully
- * customizable.  If you'd like to work with your assets differently 
+ * customizable.  If you'd like to work with your assets differently
  * you can change this file to do anything you like!
  *
  * More information on using Grunt to work with static assets:
@@ -23,8 +23,8 @@ module.exports = function (grunt) {
    * (uses Grunt-style wildcard/glob/splat expressions)
    *
    * By default, Sails also supports LESS in development and production.
-   * To use SASS/SCSS, Stylus, etc., edit the `sails-linker:devStyles` task 
-   * below for more options.  For this to work, you may need to install new 
+   * To use SASS/SCSS, Stylus, etc., edit the `sails-linker:devStyles` task
+   * below for more options.  For this to work, you may need to install new
    * dependencies, e.g. `npm install grunt-contrib-sass`
    */
 
@@ -32,18 +32,23 @@ module.exports = function (grunt) {
     'linker/**/*.css'
   ];
 
+  var lessFilesToInject = [
+    //"bower_components/bootstrap/less/bootstrap.less",
+    'styles/less/styles.less',
+    '*.less'
+  ];
 
   /**
    * Javascript files to inject in order
    * (uses Grunt-style wildcard/glob/splat expressions)
    *
-   * To use client-side CoffeeScript, TypeScript, etc., edit the 
+   * To use client-side CoffeeScript, TypeScript, etc., edit the
    * `sails-linker:devJs` task below for more options.
    */
 
   var jsFilesToInject = [
 
-    // Below, as a demonstration, you'll see the built-in dependencies 
+    // Below, as a demonstration, you'll see the built-in dependencies
     // linked in the proper order order
 
     // Bring in the socket.io client
@@ -67,8 +72,8 @@ module.exports = function (grunt) {
    * Client-side HTML templates are injected using the sources below
    * The ordering of these templates shouldn't matter.
    * (uses Grunt-style wildcard/glob/splat expressions)
-   * 
-   * By default, Sails uses JST templates and precompiles them into 
+   *
+   * By default, Sails uses JST templates and precompiles them into
    * functions for you.  If you want to use jade, handlebars, dust, etc.,
    * edit the relevant sections below.
    */
@@ -105,17 +110,17 @@ module.exports = function (grunt) {
   /////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////
 
-  // Modify css file injection paths to use 
+  // Modify css file injection paths to use
   cssFilesToInject = cssFilesToInject.map(function (path) {
     return '.tmp/public/' + path;
   });
 
-  // Modify js file injection paths to use 
+  // Modify js file injection paths to use
   jsFilesToInject = jsFilesToInject.map(function (path) {
     return '.tmp/public/' + path;
   });
-  
-  
+
+
   templateFilesToInject = templateFilesToInject.map(function (path) {
     return 'assets/' + path;
   });
@@ -200,9 +205,9 @@ module.exports = function (grunt) {
         files: [
           {
           expand: true,
-          cwd: 'assets/styles/',
-          src: ['*.less'],
-          dest: '.tmp/public/styles/',
+          cwd: 'assets/',
+          src: lessFilesToInject,
+          dest: '.tmp/public/linker/styles/',
           ext: '.css'
         }, {
           expand: true,
@@ -214,7 +219,7 @@ module.exports = function (grunt) {
         ]
       }
     },
-    
+
     coffee: {
       dev: {
         options:{
@@ -449,7 +454,7 @@ module.exports = function (grunt) {
   grunt.registerTask('default', [
     'compileAssets',
     'linkAssets',
-    'bower',    
+    'bower',
     'watch'
   ]);
 
@@ -457,7 +462,7 @@ module.exports = function (grunt) {
     'clean:dev',
     'jst:dev',
     'less:dev',
-    'copy:dev',    
+    'copy:dev',
     'coffee:dev'
   ]);
 
