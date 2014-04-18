a98b1f2fc77e5fd6f83f8df3245b957dd016c1ee
Alberto Souza <alberto.souza.99@gmail.com>
2014-04-17 21:23:15 -0300

    changed grunt bump tag pattern

--- tasks/config/grunt-bump.js
+++ tasks/config/grunt-bump.js
@@ -9,11 +9,11 @@ module.exports = function(grunt) {
       files: ['package.json', 'bower.json'],
       updateConfigs: [],
       commit: true,
-      commitMessage: '%VERSION%',
+      commitMessage: 'Release v%VERSION%',
       commitFiles: ['package.json', 'bower.json'], // '-a' for all files
       createTag: true,
-      tagName: '%VERSION%',
-      tagMessage: '%VERSION%',
+      tagName: 'v%VERSION%',
+      tagMessage: 'Version %VERSION%',
       push: true,
       pushTo: 'origin',
       gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
