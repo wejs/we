b055f548fd81664773bc73fc37ea7ec3a7cbd8a9
Alberto Souza <alberto.souza.99@gmail.com>
2014-05-09 20:38:05 -0300

    updated AllRoutesPolicy.js to prepopulate req.user form session with socket.io

--- api/policies/AllRoutesPolicy.js
+++ api/policies/AllRoutesPolicy.js
@@ -7,6 +7,41 @@ module.exports = function (req, res, ok) {
     sails.log.info('Send the index page for request: ', req.url);
     return res.view("home/index.ejs");
   }else{
-    return ok();
+
+    if(req.isSocket){
+      // Get session and current user
+      sails.session.fromSocket(req.socket, function (err, session) {
+        if(err){
+          sails.log.error(err);
+          return res.send("Forbiden", 403);
+        }
+
+        if(session && session.passport && session.passport.user){
+
+          Users.findOneById(session.passport.user).exec(function(error, user){
+            if(error){
+              sails.log.error('Error on get user from session in socket.io: ', err);
+            }
+
+            if(user){
+              req.user = user;
+            }
+
+            return ok();
+          });
+
+        }else{
+          // not authenticated ...
+          return ok();
+        }
+
+      });
+
+
+    }else{
+      return ok();
+    }
+
+
   }
 };
