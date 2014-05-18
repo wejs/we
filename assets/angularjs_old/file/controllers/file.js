2bfe88a85fac474993b876dfda53570e755d02e7
Alberto Souza <alberto.souza.99@gmail.com>
2014-05-18 03:21:00 -0300

    remove angularjs

--- assets/angularjs_old/file/controllers/file.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-  define('file/controllers/file',['angular', 'modules'], function (angular, Application) {
-
-    // @TODO
-    console.info('TODO file.js fileupload suport');
-
-  });
-}());
