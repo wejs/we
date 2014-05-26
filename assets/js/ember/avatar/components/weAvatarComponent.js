1b6c2f4bd2a694d07aca3b281faee33fcd0451c5
Alberto Souza <alberto.souza.99@gmail.com>
2014-05-26 18:18:43 -0300

    added refresh param to add query string in avatar images to force refresh

--- assets/js/ember/avatar/components/weAvatarComponent.js
+++ assets/js/ember/avatar/components/weAvatarComponent.js
@@ -6,7 +6,8 @@ define(['we','ember'], function (we) {
     defaultAvatarUrl: '/imgs/avatars/user-avatar.png',
     attributeBindings: ['userId'],
     userId: null,
-    init: function(){
+    didInsertElement: function didInsertElement() {
+
       this._super();
       var self = this;
 
@@ -15,17 +16,21 @@ define(['we','ember'], function (we) {
       // refresh avatar on user avatar change
       we.events.on('userAvatarChange',function(event, data){
         if(data.user.id === self.userId){
-          self.changeAvatarUrl();
+          self.changeAvatarUrl(true);
         }
       });
 
     },
-    changeAvatarUrl: function() {
+    changeAvatarUrl: function(refresh) {
       var userId = this.get('userId');
       if(userId){
-        // get current time to set as refresh query param
-        var refreshTime = new Date().getTime();
-        this.set('avatarUrl', '/avatar/' + userId + '?r=' + refreshTime);
+        if(refresh){
+          // get current time to set as refresh query param
+          var refreshTime = new Date().getTime();
+          this.set('avatarUrl', '/avatar/' + userId + '?r=' + refreshTime);
+        }else{
+          this.set('avatarUrl', '/avatar/' + userId);
+        }
       }else{
         this.set('avatarUrl', this.get('defaultAvatarUrl'));
       }
