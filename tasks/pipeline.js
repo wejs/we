3e156129e5ae9399e0d5225a961e4ae65a1bfd3d
Alberto Souza <alberto.souza.99@gmail.com>
2014-05-30 18:24:14 -0300

    added bootstrap-tagsinput for handle tags input/select

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -16,7 +16,8 @@ var _ = require('lodash');
 // (if you're using LESS with the built-in default config, you'll want
 //  to change `assets/styles/importer.less` instead.)
 var cssFilesToInject = [
-  'styles/**/*.css'
+  'styles/**/*.css',
+  'bower_components/bootstrap-tagsinput/dist/bootstrap-tagsinput.css'
 ];
 
 
