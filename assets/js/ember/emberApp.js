c5644ccc80fafc2abebcb3576b856ac60179a937
Alberto Souza <alberto.souza.99@gmail.com>
2014-07-09 18:34:01 -0300

    move app helpers to /ember/application/helpers/AppHelpers.js file

--- assets/js/ember/emberApp.js
+++ assets/js/ember/emberApp.js
@@ -9,13 +9,11 @@
 
 define('emberApp',[
   'we',
-  'showdown',
   'moment',
-  'async',
   'ember',
   'weEmberPlugin',
   'sails.io'
-], function (we, Showdown, moment, async) {
+], function (we, moment) {
 
   // configure moment.js
   moment.lang(we.config.language);
@@ -87,53 +85,6 @@ define('emberApp',[
 
     App.advanceReadiness();
 
-  });
-
-  var showdown = new Showdown.converter();
-
-  Ember.Handlebars.helper('format-markdown', function(input) {
-    return new Handlebars.SafeString(showdown.makeHtml(input));
-  });
-
-  Ember.Handlebars.helper('format-date', function(date) {
-    return moment(date).fromNow();
-  });
-
-
-  // Localization helper
-  if(we.config.language == "en-us"){
-    // dont translate if are the default system language
-    Ember.Handlebars.registerHelper('t', function (property, options) {
-      return property;
-    });
-  }else{
-    Ember.Handlebars.registerHelper('t', function (property, options) {
-      if(property){
-        return we.i18n( property );
-      } else{
-        return '';
-      }
-    });
-  }
-
-
-  // Limit string length
-  // usage: {{substr description max=20}}
-  // or {{substr description start=5 max=20}}
-  Ember.Handlebars.registerHelper('substr', function(property, options) {
-
-    var str = Ember.get(this, property);
-    var opts = options.hash;
-
-    var start = opts.start || 0;
-    var len = opts.max;
-
-    var out = str.substr(start, len);
-
-    if (str.length > len)
-        out += '...';
-
-    return new Handlebars.SafeString(out);
   });
 
   return App;
