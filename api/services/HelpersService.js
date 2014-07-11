b56f76801fb236c8ae52997eb28ae8d8943c0846
Alberto Souza <alberto.souza.99@gmail.com>
2014-07-11 09:13:32 -0300

    added helper getlinkCssTags to dinamic generate script link tags

--- api/services/HelpersService.js
+++ api/services/HelpersService.js
@@ -36,3 +36,28 @@ exports.getRequireJsScriptTag = function(){
   return '<script data-main="/main" src="/js/libs/require.js"></script>';
 };
 
+// TODO make this config dinamic
+exports.getlinkCssTags = function(){
+  var pipeline = require('../../tasks/pipeline.js');
+  var theme_name = sails.config.themes.enabled;
+  var theme = require(theme_name);
+  var tags = '';
+
+  if(sails.config.environment == 'production'){
+    tags += '<link rel="stylesheet" href="/min/production.css">';
+  }else{
+
+    tags += '<link rel="stylesheet" href="/styles/bootstrap.min.css">'
+    tags += '<link rel="stylesheet" href="/styles/importer.css">';
+    tags += '<link rel="stylesheet" href="/styles/theme.css">';
+    tags += '<link rel="stylesheet" href="/bower_components/font-awesome/css/font-awesome.css">';
+    tags += '<link rel="stylesheet" href="/wysiwyg/summernote/dist/summernote.css">';
+    tags += '<link rel="stylesheet" href="/bower_components/select2/select2.css">';
+    tags += '<link rel="stylesheet" href="/bower_components/codemirror/lib/codemirror.css">';
+    tags += '<link rel="stylesheet" href="/styles/theme/style.css">';
+
+
+  }
+
+  return tags;
+}
