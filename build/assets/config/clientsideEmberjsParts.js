2f66e1dc1a6c1a7507b7a1d273173a1ae6f81cfe
Alberto Souza <alberto.souza.99@gmail.com>
2014-07-12 17:10:32 -0300

    fix build filder structure

---
+++ build/assets/config/clientsideEmberjsParts.js
@@ -0,0 +1,75 @@
+/* Automatic generated file - dont edit!*/
+module.exports.clientsideEmberjsParts = {
+  "parts": {
+    "controllers": [
+      "js/ember/application/controllers/ApplicationController",
+      "js/ember/application/controllers/HomeController",
+      "js/ember/auth/controllers/AuthControllerLogin",
+      "js/ember/auth/controllers/AuthForgotPasswordController",
+      "js/ember/auth/controllers/AuthRegisterController",
+      "js/ember/avatar/controllers/AvatarChangeModalController",
+      "js/ember/avatar/controllers/AvatarImageController",
+      "js/ember/avatar/controllers/AvatarLinkImageController",
+      "js/ember/group/controllers/GroupController",
+      "js/ember/group/controllers/GroupsCreateController",
+      "js/ember/group/controllers/GroupsIndexController",
+      "js/ember/messenger/controllers/MessengerContactController",
+      "js/ember/messenger/controllers/MessengerContactListController",
+      "js/ember/messenger/controllers/MessengerMessageController",
+      "js/ember/post/controllers/PostController",
+      "js/ember/user/controllers/UserController",
+      "js/ember/user/controllers/UserMenuController"
+    ],
+    "models": [
+      "js/ember/application/models/ActivityModel",
+      "js/ember/comment/models/CommentModel",
+      "js/ember/file/models/ImageModel",
+      "js/ember/group/models/GroupModel",
+      "js/ember/post/models/PostModel",
+      "js/ember/user/models/ContactModel",
+      "js/ember/user/models/UserModel"
+    ],
+    "views": [
+      "js/ember/auth/views/AuthRegisterView",
+      "js/ember/avatar/views/AvatarChangeModalView",
+      "js/ember/avatar/views/AvatarView",
+      "js/ember/messenger/views/MessengereBoxContainerView",
+      "js/ember/user/views/ContactButtomView"
+    ],
+    "components": [
+      "js/ember/application/components/WeConnectionStatusComponent",
+      "js/ember/application/components/WeWysiwygEditorComponent",
+      "js/ember/auth/components/WeAuthModalLoginComponent",
+      "js/ember/avatar/components/WeAvatarComponent",
+      "js/ember/avatar/components/WeAvatarModalComponent",
+      "js/ember/comment/components/WeCommentFormComponent",
+      "js/ember/file/components/FileUploadComponent",
+      "js/ember/file/components/WeImagePreviewComponent",
+      "js/ember/messenger/components/WeMessengerBoxComponent",
+      "js/ember/messenger/components/WeMessengerComponent",
+      "js/ember/messenger/components/WeMessengerPublicBoxComponent",
+      "js/ember/post/components/WeShareboxComponent"
+    ],
+    "routes": [
+      "js/ember/application/routes/ApplicationRoute",
+      "js/ember/application/routes/HomeRoute",
+      "js/ember/application/routes/UnknownRoute",
+      "js/ember/auth/routes/AuthForgotPasswordRoute",
+      "js/ember/group/routes/GroupRoute",
+      "js/ember/post/routes/PostRoute",
+      "js/ember/user/routes/UserRoutes"
+    ],
+    "helpers": [
+      "js/ember/application/helpers/AppHelpers",
+      "js/ember/auth/helpers/CanHelper"
+    ]
+  },
+  "types": [
+    "controllers",
+    "models",
+    "views",
+    "components",
+    "routes",
+    "helpers"
+  ]
+};
