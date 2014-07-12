0eac810ef01894be1db3f2226b3bea3777fd3341
Alberto Souza <alberto.souza.99@gmail.com>
2014-07-12 17:56:29 -0300

    fix home register form

--- assets/js/ember/application/routes/HomeRoute.js
+++ assets/js/ember/application/routes/HomeRoute.js
@@ -4,11 +4,6 @@ define(['we','ember'], function (we) {
   App.HomeRoute = Ember.Route.extend({
     renderTemplate: function() {
       this.render('home');
-      this.render('auth/registerForm', {
-        into: 'application',
-        outlet: 'highlighted',
-        controller: 'AuthRegister'
-      });
     },
     model: function(params) {
       if(we.isAuthenticated()){
