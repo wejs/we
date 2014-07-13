7b36dfe111f1cb79428633e3631188fd2d0b84c6
Alberto Souza <alberto.souza.99@gmail.com>
2014-07-13 20:32:46 -0300

    fix usermenu

--- assets/js/ember/user/controllers/UserMenuController.js
+++ assets/js/ember/user/controllers/UserMenuController.js
@@ -3,24 +3,16 @@ define(['we','ember'], function (we) {
 
   App.UserMenuController = Ember.Controller.extend({
     isVisible: false,
-    user: {},
     init: function() {
       var self = this;
       if(we.authenticatedUser.id){
-        self.store.find('user',we.authenticatedUser.id).then(function(user){
-          self.set('user', user);
-          self.set('isVisible', true);
-        });
+        self.set('isVisible', true);
       }
       we.hooks.on("user-authenticated",function(user, done){
-        self.store.find('user',we.authenticatedUser.id).then(function(userModel){
-          self.set('user', user);
-          self.set('isVisible', true);
-        });
+        self.set('isVisible', true);
         done();
       });
       we.hooks.on("user-unauthenticated",function(user, done){
-        self.set('user', {});
         self.set('isVisible', false);
         done();
       });
