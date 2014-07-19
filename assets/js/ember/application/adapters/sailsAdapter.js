4c7b14af9c6497a3b780a370867bedbd2528536b
Alberto Souza <alberto.souza.99@gmail.com>
2014-07-19 11:55:52 -0300

    added Ember.js DS.ArrayTransform

--- assets/js/ember/application/adapters/sailsAdapter.js
+++ assets/js/ember/application/adapters/sailsAdapter.js
@@ -4,6 +4,26 @@
  *
  */
 define(['we','ember'], function (we) {
+  /* Register 'array' data type */
+  DS.ArrayTransform = DS.Transform.extend({
+    deserialize: function(serialized) {
+      return (Ember.typeOf(serialized) == "array") ? serialized : [];
+    },
+    serialize: function(deserialized) {
+    var type = Ember.typeOf(deserialized);
+      if (type == 'array') {
+          return deserialized;
+      } else if (type == 'string') {
+          return deserialized.split(',').map(function(item) {
+              return jQuery.trim(item);
+          });
+      } else {
+          return [];
+      }
+    }
+  });
+  App.register("transform:array", DS.ArrayTransform);
+
   App.ApplicationAdapter = DS.SailsRESTAdapter.extend({
     defaultSerializer: '-default',
     listeningModels: {},
