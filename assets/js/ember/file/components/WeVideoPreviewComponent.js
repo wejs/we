b6631ad1a0a0d89b17b4f237295bbed9aed29530
Alberto Souza <alberto.souza.99@gmail.com>
2014-07-19 15:14:43 -0300

    update on video preview component to use img tag

--- assets/js/ember/file/components/WeVideoPreviewComponent.js
+++ assets/js/ember/file/components/WeVideoPreviewComponent.js
@@ -2,20 +2,14 @@
 define(['we','ember'], function (we) {
   App.WeVideoPreviewComponent = Ember.Component.extend({
     //onRemove : 'onRemoveVideo',
-    tagName: 'div',
-    videoUrl: '',
-    videoHtml: '',
-    layout: Ember.Handlebars.compile("{{{videoHtml}}}"),
+    tagName: 'img',
+    src: '',
+    attributeBindings: ['src'],
+
     willInsertElement: function(){
       this._super();
       var _this = this;
       var url = this.get('url');
-
-      window.testeUrl = url;
-
-      window.teste = this;
-
-      var videoProvider = we.utils.isVideoUrl(url);
 
       var video = we.utils.parseVideoUrl(url);
 
@@ -23,32 +17,19 @@ define(['we','ember'], function (we) {
         this.set('video', video);
         switch (video.provider){
           case 'youtube':
-              //this.set('videoHtml',getYoutubeHtml(url));
-              this.set('videoUrl','http://img.youtube.com/vi/'+video.id+'/0.jpg');
-              this.set('videoHtml', this.getYoutubeHtml( this.get('videoUrl') ));
+              this.set('src','http://img.youtube.com/vi/'+video.id+'/0.jpg');
             break;
           case 'vimeo':
-            this.set('videoUrl','//player.vimeo.com/video/'+video.id);
-            //this.set('videoHtml', this.getVimeoHtml( this.get('videoUrl') ));
+            this.set('src','//player.vimeo.com/video/'+video.id);
             break;
         }
       }
     },
-    getYoutubeHtml: function(url){
-      return '<img src="'+url+'">';
-    },
-    // TODO add suport to vimeo video
-    getVimeoHtml: function(url){
-      return '<img src="'+url+'">';
-      //return '<iframe src="" width="WIDTH" height="HEIGHT" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-    },
-
     click: function(){
       if(this.get('onRemove')){
         this.sendAction('onRemove', this.get('url'), this);
       }
     }
-
   });
 });
 
