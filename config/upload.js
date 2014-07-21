9c0a1ae2b27cfb228eb8ded5e047058a06502c66
Alberto Souza <alberto.souza.99@gmail.com>
2014-07-21 02:01:18 -0300

    added default upload config

---
+++ config/upload.js
@@ -0,0 +1,36 @@
+/**
+ * File upload configs
+ * (sails.config.upload)
+ *
+ */
+
+module.exports.upload = {
+  image: {
+    avaibleStyles: [
+      'mini',
+      'thumbnail',
+      'medium',
+      'large'
+    ],
+
+    styles: {
+      mini: {
+        width: '24',
+        heigth: '24'
+      },
+      thumbnail: {
+        width: '75',
+        heigth: '75'
+      },
+      medium: {
+        width: '250',
+        heigth: '250'
+      },
+      large: {
+        width: '640',
+        heigth: '640'
+      }
+    }
+  },
+
+};
