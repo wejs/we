2d02b7763ba7ec332ccfa38c0969886ca4e5c73b
Alberto Souza <alberto.souza.99@gmail.com>
2014-07-25 14:34:07 -0300

    changed we.js clientside configs to disable development features by default

--- config/clientside.js
+++ config/clientside.js
@@ -1,3 +1,10 @@
+/**
+ * Client side configs
+ * This configs controlls we.js features and configurations
+ *
+ * @author Alberto Souza <contato@albertosouza.net>
+ * @license [url] MIT
+ */
 
 module.exports.clientside = {
   // Plugin load types define load execution order
@@ -8,14 +15,21 @@ module.exports.clientside = {
       'Extension'
     ]
   },
-
+  // enable or disable we.js client side plugins here
   pluginsDefaultEnabled: [
     'weHTTPjQuery',
     'weSocketIO',
     'weI18njs',
     'weMessenger',
-    'i18njs-log-untranslated-texts',
+    // we.js plugin logger
+    //'i18njs-log-untranslated-texts',
     'weEmberPlugin'
-  ]
+  ],
+
+  // we.js logs
+  log: {
+    events: false,
+    hooks: false
+  }
 
 };
