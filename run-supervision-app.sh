29494acfa4d520828326444b8c941479e65ee031
Alberto Souza <alberto.souza.99@gmail.com>
2014-07-30 21:33:29 -0300

    update package.json to use npm start and start with supervisor for development

--- run-supervision-app.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-# Script to run app.js with supervision and ignore folders
-supervisor -i .tmp/,.git/,node_modules/,views/,assets/ app.js
-#supervisor -i .tmp/,.git/,node_modules/,views/ -- --debug=5555 app.js
