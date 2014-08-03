212e805387a366f5854a06bb9a016363380ac9af
Alberto Souza <alberto.souza.99@gmail.com>
2014-08-03 20:00:25 -0300

    update on grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,77 +14,27 @@
 
 module.exports = function(grunt) {
 
-
-  // Load the include-all library in order to require all of our grunt
-  // configurations and task registrations dynamically.
-  var includeAll;
-  try {
-    includeAll = require('include-all');
-  } catch (e0) {
-    try {
-      includeAll = require('sails/node_modules/include-all');
-    }
-    catch(e1) {
-      console.error('Could not find `include-all` module.');
-      console.error('Skipping grunt tasks...');
-      console.error('To fix this, please run:');
-      console.error('npm install include-all --save`');
-      console.error();
-
-      grunt.registerTask('default', []);
-      return;
-    }
-  }
-
-
-  /**
-   * Loads Grunt configuration modules from the specified
-   * relative path. These modules should export a function
-   * that, when run, should either load/configure or register
-   * a Grunt task.
-   */
-  function loadTasks(relPath) {
-    // in prod env only load config copy task
-    if(relPath == './tasks/config' && grunt.cli.tasks[0] == 'prod'){
-      return includeAll({
-        dirname: require('path').resolve(__dirname, relPath),
-        filter: 'copy.js'
-      }) || {};
-    }
-
-    //console.warn(grunt);
-    return includeAll({
-      dirname: require('path').resolve(__dirname, relPath),
-      filter: /(.+)\.js$/
-    }) || {};
-  }
-
-  /**
-   * Invokes the function from a Grunt configuration module with
-   * a single argument - the `grunt` object.
-   */
-  function invokeConfigFn(tasks) {
-    for (var taskName in tasks) {
-      if (tasks.hasOwnProperty(taskName)) {
-        tasks[taskName](grunt);
+  grunt.initConfig({
+    bump: {
+      options: {
+        files: ['package.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json'], // '-a' for all files
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
       }
     }
-  }
+  });
 
+  grunt.loadNpmTasks('grunt-bump');
 
-
-
-  // Load task functions
-  var taskConfigurations = loadTasks('./tasks/config'),
-    registerDefinitions = loadTasks('./tasks/register');
-
-  // (ensure that a default task exists)
-  if (!registerDefinitions.default) {
-    registerDefinitions.default = function (grunt) { grunt.registerTask('default', []); };
-  }
-
-  // Run task functions to configure Grunt.
-  invokeConfigFn(taskConfigurations);
-  invokeConfigFn(registerDefinitions);
-
+  grunt.registerTask('up', [
+    'bump'
+  ]);
 };
