3b712f0842d133f8400ee95f8edf825b9540d241
Alberto Souza <alberto.souza.99@gmail.com>
2014-08-05 22:28:49 -0300

    fix email config

--- config/email.js
+++ config/email.js
@@ -1,47 +1,48 @@
 
 // Email config example
-/*
-module.exports.email = {
-   email: {
-    site_email: 'no-reply@wejs.org',
-    from_email: 'WE',
-    from_name:  'no-reply@wejs.org',
-    defaultService: 'Mandrill',
 
-    services: {
-      Mandrill: {
-        service: 'Mandrill',
-        type: 'SMTP',
-        host: 'smtp.mandrillapp.com',
-        port: 587,
-        auth: {
-          user: 'email@gmail.com',
-          pass: 'yourpassword'
-        }
+module.exports.email = {
+  defaultService: 'test',
+
+  /*
+  site_email: 'no-reply@wejs.org',
+  from_email: 'WE',
+  from_name:  'no-reply@wejs.org',
+  defaultService: 'Mandrill',
+
+  services: {
+    Mandrill: {
+      service: 'Mandrill',
+      type: 'SMTP',
+      host: 'smtp.mandrillapp.com',
+      port: 587,
+      auth: {
+        user: 'email@gmail.com',
+        pass: 'yourpassword'
+      }
+    },
+    Live: {
+      service: "Live",
+      type: 'SMTP',
+      host: "smtp-mail.outlook.com", // hostname
+      secureConnection: false, // TLS requires secureConnection to be false
+      port: 587, // port for secure SMTP
+      auth: {
+        user: "email@we-cms.org",
+        pass: "yourpassword"
       },
-      Live: {
-        service: "Live",
-        type: 'SMTP',
-        host: "smtp-mail.outlook.com", // hostname
-        secureConnection: false, // TLS requires secureConnection to be false
-        port: 587, // port for secure SMTP
-        auth: {
-          user: "email@we-cms.org",
-          pass: "yourpassword"
-        },
-        tls: {
-          ciphers:'SSLv3'
-        }
-      },
-      gmail: {
-        service: "gmail",
-        type: 'SMTP',
-        auth: {
-          user: "email@gmail.com",
-          pass: "yourpassword"
-        }
+      tls: {
+        ciphers:'SSLv3'
+      }
+    },
+    gmail: {
+      service: "gmail",
+      type: 'SMTP',
+      auth: {
+        user: "email@gmail.com",
+        pass: "yourpassword"
       }
     }
   }
+  */
 };
-*/
