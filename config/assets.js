96516f7894c73850eda272244bebfdb1d9fd0509
Alberto Souza <alberto.souza.99@gmail.com>
2014-08-05 21:01:57 -0300

    updated summernote css folder to bower component in sub theme

--- config/assets.js
+++ config/assets.js
@@ -6,7 +6,7 @@ module.exports.assets = {
 
   css: [
     'bower_components/font-awesome/css/font-awesome.css',
-    'wysiwyg/summernote/dist/summernote.css',
+    'bower_components/summernote/dist/summernote.css',
     'bower_components/select2/select2.css',
     'bower_components/codemirror/lib/codemirror.css',
     'bower_components/Jcrop/css/jquery.Jcrop.css'
