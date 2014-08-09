2952cbe3a941b07f18eee3a91104826ae866cc35
Alberto Souza <alberto.souza.99@gmail.com>
2014-08-09 04:11:33 -0300

    fix findOne wembed

--- api/controllers/WembedController.js
+++ api/controllers/WembedController.js
@@ -4,12 +4,39 @@
  * @description ::
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
-var wn = require('wembed-nodejs-consumer');
-var path = require('path');
-var uuid = require('node-uuid');
-var mkdirp = require('mkdirp');
+// var wn = require('wembed-nodejs-consumer');
+// var path = require('path');
+// var uuid = require('node-uuid');
+// var mkdirp = require('mkdirp');
+//
+var util = require('util');
+var actionUtil = require('../../node_modules/sails/lib/hooks/blueprints/actionUtil');
+
 
 module.exports = {
+  findOne: function findOneRecord (req, res) {
+
+    var Model = Wembed;
+    var pk = actionUtil.requirePk(req);
+    var modelName = 'wembed';
+
+    var query = Model.findOne(pk);
+    //query = actionUtil.populateEach(query, req.options);
+    query.exec(function found(err, matchingRecord) {
+      if (err) return res.serverError(err);
+      if(!matchingRecord) return res.notFound('No record found with the specified `id`.');
+      /*
+      if (sails.hooks.pubsub && req.isSocket) {
+        Model.subscribe(req, matchingRecord);
+        actionUtil.subscribeDeep(req, matchingRecord);
+      }
+      */
+
+
+      res.send({wembed: matchingRecord});
+    });
+
+  }
   // /**
   //  * Save one Wembed
   //  * @todo  add suport to select wembed thumbnail
--- api/models/Post.js
+++ api/models/Post.js
@@ -8,7 +8,6 @@
 
 module.exports = {
   schema: true,
-  emberJsExcludeFromClient: true,
   attributes: {
 
     active:{
