b0b617cf95396ada84bc0ab255b8b4a89320f5de
Alberto Souza <alberto.souza.99@gmail.com>
2014-09-10 00:37:14 -0300

    updated pt-br locale and provider.app default config

--- config/wejs.js
+++ config/wejs.js
@@ -4,13 +4,18 @@
 
 module.exports.wejs = {
   // default wembed provider for get wembeds
+  // TODO remove this config and use providers.wembed
   wembedProvider: 'http://wembed.wejs.org',
   auth: {
     maxAge: 36000000
   },
   providers: {
+    // Wembed generator server
     wembed: 'http://wembed.wejs.org',
+    // Accounts with oauth and user rest server
     accounts: 'http://accounts.wejs.org',
-    api: 'http://wejs.dev'
+    // default is null for use same host as current running project
+    // Change it for ember.js get api data from other server
+    api: null
   }
 };
