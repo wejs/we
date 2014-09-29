2a0839b74aed6069f21e80986153425149e57ae3
Alberto Souza <alberto.souza.99@gmail.com>
2014-09-29 10:36:01 -0300

    added custom update notification action

--- api/controllers/NotificationController.js
+++ api/controllers/NotificationController.js
@@ -5,6 +5,10 @@
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
+var util = require('util');
+var actionUtil = require('../../node_modules/sails/lib/hooks/blueprints/actionUtil');
+var _ = require('lodash');
+
 module.exports = {
 
   getUnreadNotificationCount: function( req, res ) {
@@ -33,5 +37,61 @@ module.exports = {
         count: count
       });
     });
+  },
+
+  /**
+   * Update one notification
+   *
+   * Only update read status attr
+   *
+   */
+  update: function updateOneRecord (req, res) {
+
+    // Look up the model
+    var Model = Notification;
+
+    // Locate and validate the required `id` parameter.
+    var pk = actionUtil.requirePk(req);
+
+    var read = req.param('read');
+
+    // Find and update the targeted record.
+    //
+    // (Note: this could be achieved in a single query, but a separate `findOne`
+    //  is used first to provide a better experience for front-end developers
+    //  integrating with the blueprint API.)
+    Model.findOne(pk).populateAll().exec(function found(err, matchingRecord) {
+
+      if (err) return res.serverError(err);
+      if (!matchingRecord) return res.notFound();
+
+      if ( matchingRecord.read == read) {
+        return res.ok({ notification: matchingRecord.read });
+      }
+
+      matchingRecord.read = read;
+
+      Model.update(pk, matchingRecord)
+      .exec(function updated(err, records) {
+
+        // Differentiate between waterline-originated validation errors
+        // and serious underlying issues. Respond with badRequest if a
+        // validation error is encountered, w/ validation info.
+        if (err) return res.negotiate(err);
+
+        // Because this should only update a single record and update
+        // returns an array, just use the first item.  If more than one
+        // record was returned, something is amiss.
+        if (!records || !records.length || records.length > 1) {
+          req._sails.log.warn(
+          util.format('Unexpected output from `%s.update`.', Model.globalId)
+          );
+        }
+
+        var updatedRecord = records[0];
+
+        res.ok({ notification: updatedRecord });
+      });// </updated>
+    }); // </found>
   }
 };
