a61b2b452e682962f830ab79d9698fa1803d34a6
albertosouza <alberto.souza.99@gmail.com>
2015-02-28 13:37:38 -0300

    start one test structure

---
+++ test/tests/generator.test.js
@@ -0,0 +1,11 @@
+var _ = require('lodash');
+var we = require('../../bin/we.js');
+
+describe('generator', function() { 
+
+  describe('blog', function() {
+    it('Should generate one blog', function (done) {
+    
+    });
+  });
+});
