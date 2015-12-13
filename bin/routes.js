69fc6f3c46de1d0d7f70754acc744fd9404892b8
albertosouza <alberto.souza.99@gmail.com>
2015-12-13 16:12:01 -0200

    add filters to we routes command

--- bin/routes.js
+++ bin/routes.js
@@ -8,7 +8,7 @@ var Table = require('cli-table');
 var helpers = require('../lib/helpers');
 var we;
 
-module.exports = function run() {
+module.exports = function run(opts) {
   we = helpers.getWe();
 
   we.bootstrap(function(err, we) {
@@ -18,21 +18,70 @@ module.exports = function run() {
       if (err) return doneAll(err);
 
       var table = new Table({
-        head: ['Method', 'Path'],
-        colWidths: [10, 60]
+        head: ['Method', 'Controller', 'Model', 'Path'],
+        colWidths: [8,18, 15, 60]
       });
+
+
+      // filter by controller name
 
       // get route lists
       var routes = Object.keys(we.routes);
+
+      // filter by controller
+      if (opts.controller && typeof opts.controller == 'string') {
+        routes = routes.filter(function (r) {
+          if (we.routes[r].controller == opts.controller) {
+            return true;
+          } else {
+            return false;
+          }
+        });
+      }
+
+      // filter by controller
+      if (opts.model && typeof opts.model == 'string') {
+        routes = routes.filter(function (r) {
+          if (we.routes[r].model == opts.model) {
+            return true;
+          } else {
+            return false;
+          }
+        });
+      }
+
+      // filter by method
+      if (opts.method && typeof opts.method == 'string') {
+        // ensures that method is in lower case
+        opts.method = opts.method.toLowerCase();
+        routes = routes.filter(function (r) {
+          if (r.indexOf(opts.method) === 0) {
+            return true
+          } else {
+            return false;
+          }
+        });
+      }
+      var p, route;
+
       // add each route
       for (var i = 0; i < routes.length; i++) {
-        var route = routes[i].split(' ');
+        route = routes[i].split(' ');
+        p = ((route.length > 1)? route[1]: route[0]);
 
-        if (route.length > 1) {
-          table.push( [route[0], route[1]] );
-        } else {
-          table.push( ['all', route[0]] );
+        // filter by path starting with [path]
+        if (opts.find) {
+          if (p.indexOf(opts.find) !== 0) {
+            continue;
+          }
         }
+
+        table.push([
+          (route.length > 1)? route[0]: 'all',
+          we.routes[routes[i]].controller || ' ',
+          we.routes[routes[i]].model || ' ',
+          p
+        ]);
       }
 
       // print the table
