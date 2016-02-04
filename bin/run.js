7b503694daf413c285fe0b62529166e8542809e1
albertosouza <alberto.souza.99@gmail.com>
2016-02-03 22:05:24 -0200

    add test command with suport to grep

--- bin/run.js
+++ bin/run.js
@@ -10,10 +10,6 @@ module.exports = function run() {
   if (helpers.checkIfAreInProject()) {
     require(process.cwd());
   } else {
-    console.error(
-      'we-core not found, possible problems: \n\n'+
-      '1- run "npm install" for install we-core and others dependencies in your project\n'+
-      '2- "we run" we.js cli command only works in project folder\n'
-    );
+    helpers.notInProjectError();
   }
 }
