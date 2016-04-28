678f66262c5cfb7fdd6efd379e28383e8d2c3b50
albertosouza <alberto.souza.99@gmail.com>
2016-04-28 05:31:03 -0300

    remove uneed getPluginsToUpdate from we routes

--- bin/routes.js
+++ bin/routes.js
@@ -14,81 +14,78 @@ module.exports = function run(opts) {
   we.bootstrap(function(err, we) {
     if (err) return doneAll(err);
 
-    we.pluginManager.getPluginsToUpdate(function (err) {
-      if (err) return doneAll(err);
 
-      var table = new Table({
-        head: ['Method', 'Controller', 'Model', 'Path'],
-        colWidths: [8,18, 15, 60]
-      });
-
-
-      // filter by controller name
-
-      // get route lists
-      var routes = Object.keys(we.routes);
-
-      // filter by controller
-      if (opts.controller && typeof opts.controller == 'string') {
-        routes = routes.filter(function (r) {
-          if (we.routes[r].controller == opts.controller) {
-            return true;
-          } else {
-            return false;
-          }
-        });
-      }
-
-      // filter by controller
-      if (opts.model && typeof opts.model == 'string') {
-        routes = routes.filter(function (r) {
-          if (we.routes[r].model == opts.model) {
-            return true;
-          } else {
-            return false;
-          }
-        });
-      }
-
-      // filter by method
-      if (opts.method && typeof opts.method == 'string') {
-        // ensures that method is in lower case
-        opts.method = opts.method.toLowerCase();
-        routes = routes.filter(function (r) {
-          if (r.indexOf(opts.method) === 0) {
-            return true
-          } else {
-            return false;
-          }
-        });
-      }
-      var p, route;
-
-      // add each route
-      for (var i = 0; i < routes.length; i++) {
-        route = routes[i].split(' ');
-        p = ((route.length > 1)? route[1]: route[0]);
-
-        // filter by path starting with [path]
-        if (opts.find) {
-          if (p.indexOf(opts.find) !== 0) {
-            continue;
-          }
-        }
-
-        table.push([
-          (route.length > 1)? route[0]: 'all',
-          we.routes[routes[i]].controller || ' ',
-          we.routes[routes[i]].model || ' ',
-          p
-        ]);
-      }
-
-      // print the table
-      console.log(table.toString());
-
-      doneAll();
+    var table = new Table({
+      head: ['Method', 'Controller', 'Model', 'Path'],
+      colWidths: [8,18, 15, 60]
     });
+
+
+    // filter by controller name
+
+    // get route lists
+    var routes = Object.keys(we.routes);
+
+    // filter by controller
+    if (opts.controller && typeof opts.controller == 'string') {
+      routes = routes.filter(function (r) {
+        if (we.routes[r].controller == opts.controller) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+    }
+
+    // filter by controller
+    if (opts.model && typeof opts.model == 'string') {
+      routes = routes.filter(function (r) {
+        if (we.routes[r].model == opts.model) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+    }
+
+    // filter by method
+    if (opts.method && typeof opts.method == 'string') {
+      // ensures that method is in lower case
+      opts.method = opts.method.toLowerCase();
+      routes = routes.filter(function (r) {
+        if (r.indexOf(opts.method) === 0) {
+          return true
+        } else {
+          return false;
+        }
+      });
+    }
+    var p, route;
+
+    // add each route
+    for (var i = 0; i < routes.length; i++) {
+      route = routes[i].split(' ');
+      p = ((route.length > 1)? route[1]: route[0]);
+
+      // filter by path starting with [path]
+      if (opts.find) {
+        if (p.indexOf(opts.find) !== 0) {
+          continue;
+        }
+      }
+
+      table.push([
+        (route.length > 1)? route[0]: 'all',
+        we.routes[routes[i]].controller || ' ',
+        we.routes[routes[i]].model || ' ',
+        p
+      ]);
+    }
+
+    // print the table
+    console.log(table.toString());
+
+    doneAll();
   });
 };
 
